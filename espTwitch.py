2802cf0112c13eae91899a7ccf2ffac533ca703b
mgolub2 <mgolub2@uw.edu>
2015-06-07 23:28:23 -0700

    Cleanup and comments

--- espTwitch.py
+++ espTwitch.py
@@ -1,3 +1,10 @@
+"""
+Super simple way to send commands to an esp8266 that is polling /move
+for commands. These commands are sent to a motorized tank running FreeRTOS,
+which will interpret the integers as movement commands
+Most of the twich code is from the pytwitchapi examples chatcli.py file,
+from here: https://raw.githubusercontent.com/Pytwitcher/pytwitcherapi/master/examples/chatcli.py
+"""
 from flask import Flask
 import threading
 import webbrowser
@@ -14,24 +21,34 @@ COAST = 0
 direction_queue = queue.Queue()
 
 class IRCClient(pytwitcherapi.IRCClient):
+    """
+    Send both public and private messages from the twitch irc 
+    stream to a mehtod that will look for valids directives,
+    then place the resulting direction number into the direction_queue
+    """
 
-    def __init__(self, *args, **kwargs):
-        super(IRCClient, self).__init__(*args, **kwargs)
-        self.current_direction = FORWARD
 
     def on_pubmsg(self, connection, event):
+        """
+        Grab messages, and send them off for processing
+        """
         super(IRCClient, self).on_pubmsg(connection, event)
         message = self.messages.get()
         print ('{0}: {1}'.format(message.source.nickname, message.text))
         self.process(message.text)
 
     def on_privmsg(self, connection, event):
-        super(IRCClient, self).on_pubmsg(connection, event)
-        message = self.messages.get()
-        print ('{0}: {1}'.format(message.source.nickname, message.text))
-        self.process(message.text)
+        """
+        Redirect private messages to be process by the public message handler
+        """
+        self.on_pubmsg(self, connection, event)
 
     def process(self, text):
+        """
+        Looks at the first character of a message for a match with 
+        f, b, r, or l to set the correpsonding direction, then 
+        send that off into the queue. 
+        """
         if text[0].lower() == 'f':
             self.current_direction = FORWARD
         elif text[0].lower() == 'b':
@@ -43,6 +60,9 @@ class IRCClient(pytwitcherapi.IRCClient):
         direction_queue.put_nowait(self.current_direction)
 
 def authorize(session):
+    """
+    Authorize with the twitch api, so we can attach the the chat stream
+    """
     session.start_login_server()
     url = session.get_auth_url()
     webbrowser.open(url)
@@ -51,17 +71,27 @@ def authorize(session):
 
 
 def create_client(session):
+    """
+    Attach a session to a channel for the IRC client to listen too.
+    """
     channel = session.get_channel('mgolub2')
     return IRCClient(session, channel)
 
 @app.route('/')
 def hello_world():
+    """
+    For that cheery optimism
+    """
     return 'Hello World!'
 
-#@app.route('')
 
 @app.route('/move')
 def move():
+    """
+    Grab the top of the directio_queue and return it. The esp8266
+    will call this method every so often to get the next movement 
+    direction
+    """
     try:
         return str(direction_queue.get_nowait())
     except queue.Empty:
@@ -70,11 +100,10 @@ def move():
         print(e)
 
 if __name__ == '__main__':
+    #Setup the twitch api, and start a seperate thread for the irc client
     session = pytwitcherapi.TwitchSession()
     authorize(session)
     client = create_client(session)
     t = threading.Thread(target=client.process_forever)
     t.start()
-    #while True:
-    #    time.sleep(10)
     app.run(host='0.0.0.0', port=8000)
